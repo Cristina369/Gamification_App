f24cd24da916ac2afb0f44313c2129343a41eabf
Cristina <cristy.wolf666@gmail.com>
2023-05-05 21:05:07 +0300

    upl

--- admin/src/App.js
+++ admin/src/App.js
@@ -14,12 +14,6 @@ import ProposedQuests from "./components/proposedquests/ProposedQuests";
 import FinishedQuests from "./components/finisedquests/FinishedQuests";
 import AcceptedQuests from "./components/acceptedquests/AcceptedQuests";
 import AllBadges from "./components/allquests/AllBadges";
-// import AllBlog from "./components/allblogs/AllBlogs";
-// import Users from "./components/users/Users";
-// import MyBlogs from "./components/blog/MyBlogs";
-// import EditBlog from "./components/blog/EditBlog";
-// import AddBlog from "./components/blog/AddBlog";
-// import UserBlog from "./components/allblogs/UserBlog";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -56,14 +50,6 @@ const App = () => {
             <Route exact path="/finished-quests" element={<FinishedQuests />} />
             <Route exact path="/accepted-quests" element={<AcceptedQuests />} />
             <Route exact path="/badges" element={<AllBadges />} />
-
-            {/* <Route exact path="/my-blogs" element={<MyBlogs />} />
-            <Route exact path="/blogs" element={<AllBlog />} />
-            <Route exact path="/users" element={<Users />} />
-            <Route exact path="/add-blog" element={<AddBlog />} />
-            <Route exact path="/edit-blog/:id" element={<EditBlog />} />
-            <Route path="/signup" element={<Login />} />
-            <Route exact path="/blog/:id" element={<UserBlog />} /> */}
           </Routes>
         </Fragment>
       )}
